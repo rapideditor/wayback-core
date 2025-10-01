5fa36b356db009bad3f94cc71a77a17ab5336236
Bryan Housel <bhousel@gmail.com>
2025-10-01 14:11:43 -0400

    Bump dependencies, remove jest and husky

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  // Automatically clear mock calls and instances between every test
-  clearMocks: true,
-  // Indicates whether each individual test should be reported during the run
-  verbose: true
-};
